b40756a9efbbe7b73f24aba15b12d45a930f393a
Kang Seokjin <tjrrh123@gmail.com>
2022-04-09 15:03:35 +0900

    JSON API - Setup

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/runan13/gocoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-func main(){
-	explorer.Start()
+	"github.com/runan13/gocoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func handleDoc(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
+
+func main() {
+	http.HandleFunc("/", handleDoc)
+
+	fmt.Printf("Listening http:localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
