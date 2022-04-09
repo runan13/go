8fdfd51f6c3db69e4e5573fd6416eb27ba3ea56c
Kang Seokjin <tjrrh123@gmail.com>
2022-04-09 14:34:55 +0900

    Add Block

--- main.go
+++ main.go
@@ -25,10 +25,23 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "home", data)
 }
 
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			templates.ExecuteTemplate(w, "add", nil)
+		case "POST":
+			r.ParseForm()
+			data := r.Form.Get("blockData")
+			blockchain.GetBlockchain().AddBlock(data)
+			http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+		}
+}
+
 func main(){
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/add", handleAdd)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port,nil))
 }
