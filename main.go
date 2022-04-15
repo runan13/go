2184381f903b02e1eaaa0bb21fc1ae4a578c5d62
Kang Seokjin <tjrrh123@gmail.com>
2022-04-15 15:53:00 +0900

    JSON Decode

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/runan13/gocoin/blockchain"
+	"github.com/runan13/gocoin/utils"
 	"log"
 	"net/http"
 )
@@ -36,6 +38,11 @@ func handleDoc(rw http.ResponseWriter, r *http.Request) {
 			Description: "Add a Block",
 			Payload:     "data:string",
 		},
+		{
+			URL:         URL("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See a Block",
+		},
 	}
 
 	rw.Header().Add("Content-Type", "application/json")
@@ -43,8 +50,26 @@ func handleDoc(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+type AddBlockBody struct {
+	Message string
+}
+
+func handleBlocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", handleDoc)
+	http.HandleFunc("/blocks", handleBlocks)
 
 	fmt.Printf("Listening http:localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
